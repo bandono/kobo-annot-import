af0c2df0509574c9d05660f247487e8c5bb939e6
bandono <arif.working@gmail.com>
2023-12-02 21:23:26 +0700

    output the annotation into individuals markdown (*.md) files

--- kobo-annot-import.py
+++ kobo-annot-import.py
@@ -1,7 +1,35 @@
+import os
+import re
+import hashlib
 import xml.etree.ElementTree as ET
 
-# Define the file path
+# Define the input file path > todo refactor
 file_path = ''  # Replace with the actual file path
+# Define result folder root > todo refactor
+folder_name = 'result'
+
+def extract_content_between_heading_and_uuid(content):
+    # Extract content between heading and UUID using regular expressions
+    match = re.search(r'#.*\n\n(.*)\n\nUUID: `.*`', content, re.DOTALL)
+
+    if match:
+        extracted_content = match.group(1)
+        return extracted_content.strip()
+    else:
+        return None
+
+def write_md_file(filename, uuid, title, author, content_text, target_text):
+        with open(filename, 'w', encoding='utf-8') as file:
+            file.write(f"---\n")
+            file.write(f"type: book-annotation\n")
+            file.write(f"book-title: {title}\n")
+            file.write(f"book-author: {author}\n")
+            file.write(f"tags: []\n")
+            file.write(f"---\n")
+            file.write(f"# {content_text.title()}\n\n")
+            file.write(f"{target_text}\n\n\n")   
+            file.write(f"UUID: `{uuid}`\n\n")
+
 
 # Parse the XML data
 tree = ET.parse(file_path)
@@ -12,6 +40,9 @@ nsmap = {'dc': 'http://purl.org/dc/elements/1.1/'}
 title = root.find(".//dc:title", namespaces=nsmap).text
 xmlns = root.tag.split('}')[0][1:]
 
+# Find author from creator
+author = root.find(".//dc:creator", namespaces=nsmap).text
+
 
 # Define the namespaces
 namespaces = {
@@ -20,34 +51,43 @@ namespaces = {
 }
 
 
-# Print the title
-print(f"Title: {title}")
-print(f"xmlns: {xmlns}")
-
 # Iterate over annotation elements
 for annotation in root.findall(".//xmlns:annotation", namespaces=namespaces):
     # Extract UUID
     uuid_element = annotation.find(".//dc:identifier", namespaces=namespaces)
     if uuid_element is not None:
         uuid = uuid_element.text.split(":")[-1]
-        # Print UUID
-        print(f"UUID: {uuid}")
 
     # Extract text under <annotation><target>
     target_text_element = annotation.find(".//xmlns:target/xmlns:fragment/xmlns:text", namespaces=namespaces)
     if target_text_element is not None:
         target_text = target_text_element.text.strip()
-        # Print Target Text
-        print(f"Target Text: {target_text}")
 
     # Extract text under <annotation><content>
     content_text_element = annotation.find(".//xmlns:content/xmlns:text", namespaces=namespaces)
     if content_text_element is not None:
         content_text = content_text_element.text.strip()
-        # Print Content Text
-        print(f"Content Text: {content_text}")
 
+    # Save data to Markdown file
+    # 1. Check if the file already exists
+    filename = os.path.join(folder_name, f"{uuid}.md")
+    if os.path.exists(filename):
+        # 1.a. Read the existing file's content
+        with open(filename, 'r', encoding='utf-8') as existing_file:
+            existing_content = existing_file.read()
+        
+        # 1.b.Compare target text content only
+        sha256_existing = hashlib.sha256(extract_content_between_heading_and_uuid(existing_content).encode()).hexdigest()
+        sha256_new = hashlib.sha256(target_text.encode()).hexdigest()
+        if sha256_existing != sha256_new:
+            # 1.c. Update file name appending suffix (1) 
+            filename = os.path.join(folder_name, f"{uuid} (1).md")
+            write_md_file(filename, uuid, title, author, content_text, target_text)
 
+    else:
+        filename = os.path.join(folder_name, f"{uuid}.md")
+        write_md_file(filename, uuid, title, author, content_text, target_text)
+     
 
 
 
